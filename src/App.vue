5166904ce81d90b3091e4f2b1ef0a35504f1f40b
Duckbanni <vincent.lanore@gmail.com>
2025-03-28 13:04:03 +0000

    Initial Vue setup

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue';
+</script>
+
+<template>
+  <header>
+    <div class="wrapper">
+      <HelloWorld msg="Hello world" />
+    </div>
+  </header>
+
+</template>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+defineProps<{
+  msg: string
+}>()
+</script>
+
+<template>
+    <h1 class="green">{{ msg }}</h1>
+</template>
+
