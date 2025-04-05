543e2377b675e0ef45e75a8c1b7eaa9728be2edf
Duckbanni <vincent.lanore@gmail.com>
2025-04-05 20:19:11 +0000

    Increasing counter + more formatting

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,30 @@
-import { setupCounter } from './counter.ts';
 import './style.css';
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
+    <p>
+      Time: 0
+    </p>
     <button>Explore</button>
     <button>Forage</button>
-    <button>Rest</button>
+    <button>Hunt</button>
   </div>
 `;
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!);
+
+let count: number = 0;
+
+
+setInterval(() => {
+  count += 1;
+  document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+  <div>
+    <p>
+      Time: ${count}
+    </p>
+    <button>Explore</button>
+    <button>Forage</button>
+    <button>Hunt</button>
+  </div>
+`;
+}, 100);
