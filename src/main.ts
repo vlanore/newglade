62c8fad4abd514e7c28846e031cb4dd267880c70
Duckbanni <vincent.lanore@gmail.com>
2025-05-30 10:03:52 +0000

    Raising zombies

--- src/main.ts
+++ src/main.ts
@@ -13,4 +13,11 @@ function update_xp() {
   xp_meter.innerHTML = `${state.xp} / ${state.max_xp}`;
 }
 
-setInterval(() => { state.xp += 1; update_xp(); }, 250);
+setInterval(() => { state.xp += 1; update_xp(); }, 500);
+
+let rz_button = document.getElementById("raise-zombie");
+if (rz_button == null) { throw new Error(); }
+rz_button.onclick = () => {
+  let zombies_counter = document.getElementById("zombies");
+  zombies_counter.innerHTML = `1`;
+};
