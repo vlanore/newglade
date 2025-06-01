ca91d23126c91c38f5a99bf50d670ea7a78e9898
Duckbanni <vincent.lanore@gmail.com>
2025-06-01 09:44:47 +0000

    ts warning fix

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import './style.css';
 
+declare const bootstrap: { Tooltip: any; };
 
 const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]');
 [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl));
